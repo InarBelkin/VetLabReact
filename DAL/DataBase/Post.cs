47b6d9bb961a1a9fa44a2a4d78c248470ed79aeb
Инар Инарыч <Inar2000s@mail.ru>
2021-04-13 18:51:48 +0300

    Добавьте файлы проекта.

---
+++ DAL/DataBase/Post.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Database
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        [ForeignKey("UserId")]
+        public virtual User User{ get; set; }
+
+        public int ThemeId { get; set; }
+        [ForeignKey("ThemeId")]
+        public virtual Theme Theme { get; set; }
+
+
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+    }
+}
---
+++ DAL/DataBase/Theme.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Database
+{
+    public class Theme
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual List<Post> Posts { get; set; }
+    }
+}
